fff299833250f88db03a45e730e3ad184256d00d
Paul Caoile <39387011+m2paulc@users.noreply.github.com>
2024-08-06 18:12:07 -0400

    implement delete task

--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -1,14 +1,20 @@
 import { cn } from "@/app/lib/utils";
 import { useStore } from "../store";
+import { TrashIcon } from "@heroicons/react/24/solid";
 
 export default function Task({ title }: { title: string }) {
-	// let status = "Ongoing";
 	const task = useStore((store) =>
 		store.tasks.find((task) => task.title === title)
 	);
+	const deleteTask = useStore((store) => store.deleteTask);
 	return (
 		<div className="bg-slate-200 rounded-md p-2 text-black my-4">
-			<p>{task?.title}</p>
+			<div className="flex items-center justify-between p-2">
+				<p>{task?.title}</p>
+				<button onClick={() => deleteTask(title)} className="size-5">
+					<TrashIcon />
+				</button>
+			</div>
 			<div className="flex justify-end items-center mt-2">
 				<p
 					className={cn("p-2 rounded-md text-xs", {
--- app/store.ts
+++ app/store.ts
@@ -10,10 +10,15 @@ interface Task {
 interface StoreState {
 	tasks: Task[];
 	addTask: (title: string, state: string) => void;
+	deleteTask: (title: string) => void;
 }
 
 export const useStore = create<StoreState>((set) => ({
 	tasks: [],
 	addTask: (title, state) =>
 		set((store) => ({ tasks: [...store.tasks, { title, state }] })),
+	deleteTask: (title) =>
+		set((store) => ({
+			tasks: store.tasks.filter((task) => task.title !== title),
+		})),
 }));
